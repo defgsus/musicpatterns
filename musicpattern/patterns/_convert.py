fa83503efd2960cc4c16e2a29c2881805b63c995
bergi <s.berke@netzkolchose.de>
2019-11-24 14:42:57 +0100

    add comment

--- musicpattern/patterns/_convert.py
+++ musicpattern/patterns/_convert.py
@@ -2,6 +2,12 @@ from ._inspect import is_iterable
 
 
 def convert_to_list(pattern):
+    """
+    Convert iterable to a list.
+    Nested iterables are converted to lists as well.
+    :param pattern: any iterable
+    :return: list
+    """
     assert is_iterable(pattern), "%s is not iterable" % type(pattern).__name__
 
     def _convert(obj):
